5cbc6fbf726c7924dfe0ad879d70bda999833e3e
sesam-me <lsi6601@gmail.com>
2024-04-02 23:06:55 +0900

    디자인패턴

---
+++ deisgnPattern/src/test/java/com/example/deisgnpattern/DeisgnPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deisgnpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeisgnPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
